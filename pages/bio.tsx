9d51a030b1d79f33e405272b66330483512153fd
Bjorn at Iona <bjornatwork@Macintosh.local>
2022-03-17 12:38:51 +0200

    Working blog feed

---
+++ pages/bio.tsx
@@ -0,0 +1,9 @@
+const Bio = () => {
+    return (
+        <div className="mt-3">
+            <p className="display-4 text-center">I'm Next.js</p>
+            <p className="text-center">Some interesting facts about Next.js</p>
+        </div>
+    )
+}
+export default Bio
---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,57 @@
+import {serialize} from 'next-mdx-remote/serialize'
+import {MDXRemote} from 'next-mdx-remote'
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+
+import {Button, Nav} from '../../components'
+import {Component} from "react";
+
+const components = {Nav, Button, SyntaxHighlighter}
+
+class PostPage extends Component<{ frontMatter: any, mdxSource: any }> {
+    render() {
+        let {frontMatter: {title, date}, mdxSource} = this.props;
+        return (
+            <div className="mt-4">
+                <h1>{title}</h1>
+                <MDXRemote {...mdxSource} components={components}/>
+            </div>
+        )
+    }
+}
+
+const getStaticPaths = async () => {
+    const files = fs.readdirSync(path.join('posts'))
+
+    const paths = files.map(filename => ({
+        params: {
+            slug: filename.replace('.mdx', '')
+        }
+    }))
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+const getStaticProps = async ({params: {slug}}) => {
+    const markdownWithMeta = fs.readFileSync(path.join('posts',
+        slug + '.mdx'), 'utf-8')
+
+    const {data: frontMatter, content} = matter(markdownWithMeta)
+    const mdxSource = await serialize(content)
+
+    return {
+        props: {
+            frontMatter,
+            slug,
+            mdxSource
+        }
+    }
+}
+
+export {getStaticProps, getStaticPaths}
+export default PostPage
